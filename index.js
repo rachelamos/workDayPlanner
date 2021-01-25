9b7110a7040ca31c5825c33971767060fcfef1a8
Rachel Amos <rachelamos35@gmail.com>
2021-01-25 17:53:04 -0600

    adding final updates from Sunday's work

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-var date = moment().format('MMMM Do YYYY, h:mm a');
+//Set the current date to the work day calendar
+var date = moment().format('MMMM Do YYYY');
 $('#currentDay').html(date);
+
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+var currentTime = moment().getHours();
+
+function colorCode() {
+    for (var i = 0; i <10; i++)
+    if (currentTime < i)
+}; 
+
+
+//When clicked, save button stores text into local storage.
+$(document).ready(function () {
+    $(".saveBtn").on("click", function (){
+
+    })
+});
+
+// Walter's note on getting getStorage to save
+// Store
+// localStorage.setItem("lastname", "Smith");
+// Retrieve
+// document.getElementById("result").innerHTML = localStorage.getItem("lastname");
