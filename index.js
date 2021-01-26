e14dfdd59b96145199f157fe146d4a79064bf3fa
Rachel Amos <rachelamos35@gmail.com>
2021-01-26 17:49:08 -0600

    adding working on click function and local storage

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ $('#currentDay').html(date);
 
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
-var currentTime = moment().getHours();
 
-function colorCode() {
-    for (var i = 0; i <10; i++)
-    if (currentTime < i)
-}; 
+// var currentTime = moment().getHours();
+
+// function colorCode() {
+//     for (var i = 0; i <10; i++)
+//     if (currentTime < i)
+// };
 
 
 //When clicked, save button stores text into local storage.
 $(document).ready(function () {
     $(".saveBtn").on("click", function (){
+        var event = $(this).siblings('.description').val();
+        var hour = $(this).parent().attr('id');
 
+        // saves 'events' into local storage
+        localStorage.setItem(hour, event);
     })
+
+    $('#nineAm .description').val(localStorage.getItem("nineAm"))
+    console.log($('#nineAm .description').val(localStorage.getItem("nineAm")))
+    
 });
 
 // Walter's note on getting getStorage to save
